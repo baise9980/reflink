329ca3d3f23f07eb8586ffbdfdc00adf8c9aadc6
baise9980 <40777905+baise9980@users.noreply.github.com>
2018-07-03 01:59:42 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -19,22 +19,19 @@ $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
 // pulisco il messaggio ricevuto togliendo eventuali spazi prima e dopo il testo
 $text = trim($text);
-
 $proprietario = "";
 // gestisco la richiesta
 $response = "";
 if(isset($message['text']))
-
 {
   //NUOVO PARSER:
   $text_url_array = parse_text($text);
-
   if(strpos($text, "/start") === 0 )
   {
 	$response = "Ciao $firstname! \nMandami un link Amazon preceduto da /link! \nTi rispondero' con un link affiliate!";
   }
   elseif(strpos($text, "/link") === 0 && strlen($text)>6 )
-  {	  
+  {
 	//new parser:
 	$url_to_parse = $text_url_array[1];
 	$url_affiliate = set_referral_URL($url_to_parse);
@@ -45,10 +42,10 @@ if(isset($message['text']))
 	$obj_desc = $text_url_array[0];
 	$short = make_bitly_url($url_affiliate,'ghir0','json');
 	$response = "Ecco il link di $proprietario: $obj_desc\n$worldsym  $short";
-	
+
   }
   elseif(strpos($text, "/mike") === 0 && strlen($text)>6 )
-  {	  
+  {
 	//new parser:
 	$url_to_parse = $text_url_array[1];
 	$url_affiliate = set_mike_referral_URL($url_to_parse);
@@ -58,11 +55,11 @@ if(isset($message['text']))
 	$worldsym = json_decode('"\uD83C\uDF0F"');
 	$obj_desc = $text_url_array[0];
 	$short = make_bitly_url($url_affiliate,'ghir0','json');
-	$response = "Ecco il link di Archdart: $obj_desc\n$worldsym  $short";
-	
+	$response = "Ecco il link di Mike: $obj_desc\n$worldsym  $short";
+
   }
   elseif(strpos($text, "/dc") === 0 && strlen($text)>6 )
-  {	  
+  {
 	//new parser:
 	$url_to_parse = $text_url_array[1];
 	$url_affiliate = set_dc_referral_URL($url_to_parse);
@@ -72,8 +69,8 @@ if(isset($message['text']))
 	$worldsym = json_decode('"\uD83C\uDF0F"');
 	$obj_desc = $text_url_array[0];
 	$short = make_bitly_url($url_affiliate,'ghir0','json');
-	$response = "Ecco il link di Archdart: $obj_desc\n$worldsym  $short";
-	
+	$response = "Ecco il link di Diego: $obj_desc\n$worldsym  $short";
+
   }
    elseif(strpos($text, "/link") === 0 && strlen($text)<6 )
   {
@@ -82,49 +79,48 @@ if(isset($message['text']))
 }
 /*
 *
-* prende un link amazon, estrapola l'ASIN e ricrea un link allo stesso prodotto con il referral 
+* prende un link amazon, estrapola l'ASIN e ricrea un link allo stesso prodotto con il referral
 */
 function set_referral_URL($url){
-	$referral = array("giochidigiorn-21");
+	$referral = array("miketama-21","s1m0nex27-21","antonio99-21","antcaiazza-21");
 	$random = mt_rand(0,3);
 	$GLOBALS['proprietario'] = $referral[$random];
 	$url_edited = "";
 	$parsed_url_array = parse_url($url);
-	
+
 	$seller = strstr($parsed_url_array['query'], 'm=');
-	
+
 	$parsed = extract_unit($fullstring, 'm=', '&');
 	$seller = "&".$seller;
 	$url_edited = "https://www.amazon.it".$parsed_url_array['path']."?tag=".$referral[$random].$seller;
 	return $url_edited;
 }
 function set_mike_referral_URL($url){
-	$referral_mike = "giochidigiorn-21";
-	
+	$referral_mike = "miketama-21";
+
 	$url_edited = "";
 	$parsed_url_array = parse_url($url);
-	
+
 	$seller = strstr($parsed_url_array['query'], 'm=');
-	
+
 	$parsed = extract_unit($fullstring, 'm=', '&');
 	$seller = "&".$seller;
 	$url_edited = "https://www.amazon.it".$parsed_url_array['path']."?tag=".$referral_mike.$seller;
 	return $url_edited;
 }
 function set_dc_referral_URL($url){
-	$referral_dc = "giochidigiorn-21";
-	
+	$referral_dc = "amazonscont0e-21";
+
 	$url_edited = "";
 	$parsed_url_array = parse_url($url);
-	
+
 	$seller = strstr($parsed_url_array['query'], 'm=');
-	
+
 	$parsed = extract_unit($fullstring, 'm=', '&');
 	$seller = "&".$seller;
 	$url_edited = "https://www.amazon.it".$parsed_url_array['path']."?tag=".$referral_dc.$seller."keywords=I%20Consigli%20di%20Diegocampy.it";
 	return $url_edited;
 }
-
 //nuovo parser
 function parse_text($string){
 	$string1 = str_replace("/dc", "", $string);
@@ -137,7 +133,7 @@ function parse_text($string){
 	$text_parsed = array($text_parsed_TEXT, $text_parsed_URL);
 	return $text_parsed;
 }
- 
+
 function extract_unit($string, $start, $end){
 	$pos = stripos($string, $start);
 	$str = substr($string, $pos);
@@ -151,11 +147,11 @@ function make_bitly_url($url,$login,$format = 'xml',$version = '2.0.1')
 {
 	//create the URL
 	$bitly = 'http://api.bit.ly/shorten?version='.$version.'&longUrl='.urlencode($url).'&login='.$login.'&apiKey=R_c7d78316d223d5a1d7827d58d80e76be'.'&format='.$format;
-	
+
 	//get the url
 	//could also use cURL here
 	$response = file_get_contents($bitly);
-	
+
 	//parse depending on desired format
 	if(strtolower($format) == 'json')
 	{
@@ -168,7 +164,6 @@ function make_bitly_url($url,$login,$format = 'xml',$version = '2.0.1')
 		return 'http://bit.ly/'.$xml->results->nodeKeyVal->hash;
 	}
 }
-
 /*function get_string_between($string, $start, $end){
 	$string = ' ' . $string;
 	$ini = strpos($string, $start);
@@ -177,7 +172,6 @@ function make_bitly_url($url,$login,$format = 'xml',$version = '2.0.1')
 	$len = strpos($string, $end, $ini) - $ini;
 	return substr($string, $ini, $len);
 }
-
 function clean_for_URL($string){
 	$cleaned_string = explode(' ',strstr($string,'https://'))[0];
 	if(strcmp($cleaned_string,"false") == "0"){ $cleaned_string = explode(' ',strstr($string,'http://'))[0]; }
